5f9ea6c4874b0fd89310f011e4d2edc91e7956d3
Leo Monti <leomontigatti@gmail.com>
2025-06-07 19:50:10 -0300

    fix errors

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,11 +3,7 @@
 import { useState, useEffect } from "react";
 import styles from "./Footer.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faGithub,
-  faLinkedin,
-  faTwitter,
-} from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
   const [year, setYear] = useState<number | null>(null);
--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -3,7 +3,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 import {
   faWordpress,
-  faJs,
   faPython,
   faGit,
   faJava,
